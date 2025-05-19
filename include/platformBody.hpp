ca14271e3e477c3ec6b6a9eee4336d6d8a389f6c
Komi_ui <egagamaojecer@gmail.com>
2025-05-19 12:24:00 +0800

    remove space and comments

--- include/platformBody.hpp
+++ include/platformBody.hpp
@@ -1,52 +1,59 @@
 #ifndef PLATFORMBODY_HPP
 #define PLATFORMBODY_HPP
+
 #include <SFML/System/Vector2.hpp>
-#include <SFML/System/Clock.hpp>
+#include <SFML/Graphics/Rect.hpp> 
+#include "PhysicsTypes.hpp" 
 
-/**
-* phys namespace to encapsulate all physics-related code.
-*/
-namespace phys
-{
-	class platformBody
-	{
+namespace phys {
+
+	class PlatformBody {
 	public:
+		PlatformBody(
+            unsigned int id = 0,
+            const sf::Vector2f& position = {0.f, 0.f},
+            float width = 32.f,
+            float height = 32.f,
+            bodyType type = bodyType::platform, 
+            bool initiallyFalling = false,
+            const sf::Vector2f& surfaceVelocity = {0.f, 0.f} 
+        );
 
-		////////////////////////////////////////////////////////////
-		/// \brief Constructor
-		///
-		/// Creates a platform body, defaults to a static body (m_type = 0).
-		/// Width and height are set to 32 by default, positional
-		/// attributes are initialized at sf::Vector2f(0,0).
-		///
-		////////////////////////////////////////////////////////////
-		platformBody();
+        void update(float deltaTime);
 
-		////////////////////////////////////////////////////////////
-		/// \brief Default destructor
-		///
-		////////////////////////////////////////////////////////////
+		// --- Accessors (Getters) ---
+		unsigned int getID() const { return m_id; }
+		const sf::Vector2f& getPosition() const { return m_position; }
+		float getWidth() const { return m_width; }
+		float getHeight() const { return m_height; }
+		sf::FloatRect getAABB() const;
+		bodyType getType() const { return m_type; }
+		bool isFalling() const { return m_falling; } // Renamed for clarity
+		const sf::Vector2f& getSurfaceVelocity() const { return m_surfaceVelocity; }
 
-		~platformBody();
+        // --- Mutators (Setters) ---
+        // Typically, platform properties are static after creation, but setters can be added if needed.
+        void setPosition(const sf::Vector2f& position) { m_position = position; } // If platforms can move
+        void setFalling(bool falling) { m_falling = falling; }
+        // ... others if necessary
 
-	public:
+	private:
+		unsigned int m_id;
+		sf::Vector2f m_position;
+        sf::Vector2f m_velocity; // Add if m_falling or type::moving actually moves it
+		float m_width;
+		float m_height;
+		bodyType m_type;
+		bool m_falling;
+		sf::Vector2f m_surfaceVelocity; // Now a vector for more flexibility
 
-		////////////////////////////////////////////////////////////
-		/// \brief Member data
-		////////////////////////////////////////////////////////////
+        // If m_falling implies gravity, it might have:
+        // static const sf::Vector2f GRAVITY; (defined in .cpp)
 
-        unsigned int m_id;///< unique identifier for the platform
-		sf::Vector2f m_position;///< Coordinates describing the position of the platform body
-		float m_width;///< Width of the platform body
-		float m_height;///< Height of the platform body
-		int m_type;///< The type of platform body
-		bool m_collisionTop;///< Collision information - top
-		bool m_collisionBottom;///< Collision information - bottom
-		bool m_collisionLeft;///< Collision information - left
-		bool m_collisionRight;///< Collision information - right
-		bool m_falling;///< Flag for a falling platform
-		float m_surfaceVelocity;///< The velocity interacting with objects in contact with the surface
+        // For type::vanishing or timed behaviors, store remaining time, not a clock:
+        // float m_vanishTimer = -1.f; // -1 means inactive/permanent
 	};
-}
 
-#endif //PLATFORMBODY_HPP
+} // namespace phys
+
+#endif // PLATFORMBODY_HPP
--- include/tile.hpp
+++ include/tile.hpp
@@ -1,17 +1,70 @@
 #ifndef TILE_HPP
 #define TILE_HPP
 
-#include <SFML/Graphics.hpp>
+#include <SFML/Graphics/RectangleShape.hpp>
+#include <SFML/System/Time.hpp>
+#include <SFML/Graphics/RenderTarget.hpp>
+#include <SFML/Graphics/RenderStates.hpp>
+#include <SFML/Graphics/Drawable.hpp>
+#include <SFML/Graphics/Transformable.hpp> // Added for completeness
 
-class tile : public sf::RectangleShape
-{
-    public:
-        tile();
-        ~tile();
+class Tile : public sf::Drawable, public sf::Transformable {
+public:
+    Tile(const sf::Vector2f& size = {32.f, 32.f},
+         const sf::Color& color = sf::Color::White);
 
-    public:
-        bool m_falling;
-        sf::Time m_fallTime;
+
+
+    void startFalling(sf::Time delay);/* {
+        if (!m_isFalling && !m_hasFallen) {
+            m_fallDelayTimer = delay;
+            if (m_fallDelayTimer <= sf::Time::Zero) { // Corrected: check m_fallDelayTimer
+                m_isFalling = true;
+            }
+        }
+    }*/
+
+
+    void update(sf::Time deltaTime);/* { // Single definition here
+        if (m_hasFallen) return;
+
+        if (!m_isFalling && m_fallDelayTimer > sf::Time::Zero) {
+            m_fallDelayTimer -= deltaTime;
+            if (m_fallDelayTimer <= sf::Time::Zero) {
+                m_isFalling = true;
+            }
+        }
+
+        if (m_isFalling) {
+            // 'this->' is optional for member function calls or sf::Transformable methods
+            move(0.f, m_fallSpeed * deltaTime.asSeconds()); // Visual movement
+            if (getPosition().y > 800.f) { // Some arbitrary off-screen limit (adjust as needed)
+                m_hasFallen = true; // Mark as visually "done"
+                // m_isFalling = false; // Optionally stop processing fall once hasFallen
+            }
+        }
+    }*/
+
+    bool isFalling() const { return m_isFalling; }
+    bool hasFallen() const { return m_hasFallen; } // Added a getter for m_hasFallen
+
+    void setFillColor(const sf::Color& color) { m_shape.setFillColor(color); }
+    const sf::Color& getFillColor() const { return m_shape.getFillColor(); }
+    void setTexture(const sf::Texture* texture, bool resetRect = false) { m_shape.setTexture(texture, resetRect); }
+
+    sf::FloatRect getGlobalBounds() const;
+    sf::FloatRect getLocalBounds() const;
+
+
+private:
+    virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const override;
+
+    sf::RectangleShape m_shape;
+    bool m_isFalling;
+    sf::Time m_fallDelayTimer;
+    bool m_hasFallen;
+    float m_fallSpeed;
+    sf::Time m_visualFallTimerHelper;
 };
 
 #endif // TILE_HPP
