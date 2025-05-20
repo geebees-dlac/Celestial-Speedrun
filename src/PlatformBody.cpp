931291c6fd2c2a258ed37bac38b65f887f596568
geebees <geeemm218@gmail.com>
2025-05-20 20:24:43 +0800

    fIX 2

---
+++ src/PlatformBody.cpp
@@ -0,0 +1,25 @@
+#include "PlatformBody.hpp"
+#include "PhysicsTypes.hpp" 
+
+namespace phys { 
+PlatformBody::PlatformBody(
+    unsigned int id, const sf::Vector2f& position, float width, float height,
+    bodyType type, bool initiallyFalling, const sf::Vector2f& surfaceVelocity)
+    : m_id(id),
+      m_position(position),
+      m_velocity({0.f, 0.f}), // Initialize if present
+      m_width(width),
+      m_height(height),
+      m_type(type),
+      m_falling(initiallyFalling),
+      m_surfaceVelocity(surfaceVelocity) {}
+
+sf::FloatRect PlatformBody::getAABB() const {
+    return sf::FloatRect(m_position.x, m_position.y, m_width, m_height);
+}
+
+void PlatformBody::update(float deltaTime) {
+    // Implement specific update logic for later
+}
+
+} 
